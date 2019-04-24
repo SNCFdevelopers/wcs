f3d842e357b083ec69ef36f6bd1a148a0ac4ff7a
Antoine Petot <antoine7104@gmail.com>
2019-04-24 16:50:13 +0200

    WCS Tabs : Implement v1

---
+++ src/components/tab/tab.tsx
@@ -0,0 +1,16 @@
+import { Component } from '@stencil/core';
+
+@Component({
+    tag: 'wcs-tab',
+})
+export class Tabs {
+
+
+    render() {
+        return (
+            <p>
+                My name is
+            </p>
+        );
+    }
+}
