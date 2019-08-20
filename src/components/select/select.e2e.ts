c69ee235c8cedfe6646ea570a05845c755b3dec2
Antoine Petot <antoine7104@gmail.com>
2019-08-20 10:50:10 +0200

    (select): add test for select option click

--- src/components/select/select.e2e.ts
+++ src/components/select/select.e2e.ts
@@ -20,5 +20,31 @@ describe('select', () => {
         // Then
         expect(wrapper).toHaveClass('expanded');
     });
+
+    it('Should handle option click', async () => {
+        // Given
+        const page = await newE2EPage();
+        await page.setContent(`
+            <wcs-select>
+                <wcs-select-option value="1">One</wcs-select-option>
+                <wcs-select-option value="2">Two</wcs-select-option>
+                <wcs-select-option value="3">Three</wcs-select-option>
+            </wcs-select>
+        `);
+        const select = await page.find('wcs-select');
+        const firstSelectOption = await page.find('wcs-select > wcs-select-option');
+        const label = await page.find('wcs-select >>> .wcs-select-text');
+
+        // When
+        await select.click();
+        await firstSelectOption.click();
+
+        // Then
+        expect(select).toHaveAttribute('value');
+        expect(select.getAttribute('value')).toBe('1');
+        expect(label.innerText).toBe('One');
+    });
+
 });
+
 
