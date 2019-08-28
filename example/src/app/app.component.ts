b3caebbbb9158513d71297e796f7f2b42d19f16c
Antoine Petot <antoine7104@gmail.com>
2019-08-28 16:09:57 +0200

    feat(form-field): Form field wraps input
    Remove wcs-input component, the input is now to
    be used by wrapping with a form field

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,16 +1,53 @@
 import { Component } from '@angular/core';
 
-import { SelectChangeEventDetail } from '../../../dist/types/interface';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+      <wcs-card>
+          <wcs-card-body>
+              <wcs-select id="lazy-loaded-select" placeholder="Le select" multiple>
+                  <wcs-select-option *ngFor="let opt of options" [value]="opt.value">{{ opt.name }}</wcs-select-option>
+              </wcs-select>
+              <wcs-button (click)="addOptions()" mode="stroked" class="primary">Add option</wcs-button>
+          </wcs-card-body>
+      </wcs-card>
+      <wcs-card>
+          <wcs-card-body>
+              <wcs-form-field>
+                  <input [(ngModel)]="inputValue"/>
+              </wcs-form-field>
+              <wcs-form-field>
+                  <input [(ngModel)]="inputValue"/>
+                  <wcs-button shape="square" slot="suffix"><i class="material-icons">person</i></wcs-button>
+              </wcs-form-field>
+              <wcs-form-field>
+                  <wcs-select placeholder="Et ouais">
+                      <wcs-select-option *ngFor="let opt of options" [value]="opt.value">{{ opt.name }}</wcs-select-option>
+                  </wcs-select>
+                  <input [(ngModel)]="inputValue"/>
+                  <wcs-button shape="square" slot="suffix"><i class="material-icons">person</i></wcs-button>
+              </wcs-form-field>
+          </wcs-card-body>
+      </wcs-card>
+  `,
+  styles: [
+      `
+      wcs-form-field {
+          margin-bottom: 8px;
+      }
+      input {
+          width: 100%;
+      }
+      `
+  ]
 })
 export class AppComponent {
   id = 2;
+  inputValue = `lol`;
 
-  options: { value: string, name: string }[] = [];
+  options: { value: string, name: string }[] = [{
+    name: '1', value: '1'
+  }];
 
   addOptions() {
     this.options.push({ name: this.id.toString(), value: this.id.toString() });
--- src/components/input/input-interface.tsx
+++
@@ -1,5 +0,0 @@
-export interface InputChangeEventDetail {
-    value: string | undefined | null;
-
-}
-export type TextFieldTypes = 'date' | 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'url' | 'time';
