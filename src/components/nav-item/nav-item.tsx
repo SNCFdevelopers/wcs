a0487e5cb1179e1bfdbd7e3921d793b5b4ffd7e6
MonsieurMan <ange.g.picard@gmail.com>
2019-11-08 15:37:28 +0100

    feat(nav): add href attributes to nav-items

--- src/components/nav-item/nav-item.tsx
+++ src/components/nav-item/nav-item.tsx
@@ -8,18 +8,28 @@ export class NavItem implements ComponentInterface {
     /**
      * This attribute specify the text of the item.
      */
-    @Prop({ reflect: true }) text: string = '';
+    @Prop({ reflect: true })
+    text: string = '';
 
     /**
      * Wether this item is the current selected one.
      * Use this when a user was routed to the corresponding page.
      */
-    @Prop({ reflect: true, mutable: true }) active: boolean = false;
+    @Prop({ reflect: true, mutable: true })
+    active: boolean = false;
+
+    /**
+     * Attributes mapped to a <a> tag.
+     *
+     * Don't forget to specify [routerLink] if using in conjuction with angular router.
+     */
+    @Prop({ reflect: true, mutable: true })
+    href: string;
 
     render() {
         return (
-            <a href="#" class="wcs-nav-item-container">
-                <slot/>
+            <a href={this.href} class="wcs-nav-item-container">
+                <slot />
                 <span class="wcs-nav-item-text">
                     {this.text}
                 </span>
