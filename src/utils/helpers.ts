a16d6b1801ef5c7f92ae09876d2bc5cc68736e44
Antoine Petot <antoine7104@gmail.com>
2019-11-10 18:42:06 +0100

    fix(select): do not close the select when the user clicks on scrollbar (#95)

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,8 @@
+export function isElement(element: any): element is Element {
+    return 'clientWidth' in element
+        && 'clientHeight' in element;
+}
+
 export function hasShadowDom(el: HTMLElement) {
     return !!el.shadowRoot && !!(el as any).attachShadow;
 }
