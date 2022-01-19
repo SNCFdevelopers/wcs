437021da8e8e9dfca3391599d2fd8bd05d8a0a46
FMDE09201 <m.deloge@sncf.fr>
2022-01-19 17:18:18 +0100

    docs: add field story

---
+++ stories/field/field-documentation.md
@@ -0,0 +1,1 @@
+This component can be used to display the details of an entity. It is built around the label and the content of the field
---
+++ stories/field/field.stories.ts
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/web-components';
+
+// @ts-ignore
+import fieldDocumentation from './field-documentation.md'
+import { html } from 'lit-html';
+
+export default {
+    title: 'Example/Field',
+    component: 'wcs-field',
+    parameters: {
+        docs: {
+            description: {
+                component: fieldDocumentation
+            }
+        }
+    }
+} as Meta;
+
+const Template: Story = () => html`
+    <wcs-field>
+        <wcs-field-label>Le nom du champs</wcs-field-label>
+        <wcs-field-content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa mi, hendrerit dictum dui sed, porttitor ullamcorper diam.</wcs-field-content>
+    </wcs-field>
+    <wcs-field>
+        <wcs-field-label>Autre label</wcs-field-label>
+        <wcs-field-content>In hac habitasse platea dictumst.</wcs-field-content>
+    </wcs-field>
+`;
+
+export const Default = Template.bind({});
