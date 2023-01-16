ff98cc0ebe9c13f13330457212a1ad21a215768f
Rémi MARTINEZ <remi.martinez-castro@sncf.fr>
2023-01-16 15:54:14 +0100

    feat(textarea): add missing formly properties

--- angular/projects/wcs-formly/src/lib/formly-wcs-field-textarea.component.ts
+++ angular/projects/wcs-formly/src/lib/formly-wcs-field-textarea.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 import { FieldType, FieldTypeConfig } from '@ngx-formly/core';
 import { WcsFormlyFieldWrapperProps } from './formly-wcs-field-wrapper.component';
 
-export type WcsFormlyTextareaProps = WcsFormlyFieldWrapperProps;
+export type WcsFormlyTextareaProps = WcsFormlyFieldWrapperProps & {
+  autocapitalize?: 'off' | 'none' | 'on' | 'sentences' | 'words' | 'characters',
+  autofocus?: boolean,
+  autoGrow?: boolean,
+  spellcheck?: boolean,
+  wrap?: 'hard' | 'soft' | 'off',
+};
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -17,6 +23,15 @@ export type WcsFormlyTextareaProps = WcsFormlyFieldWrapperProps;
         [formlyAttributes]="field"
         [placeholder]="props.placeholder"
         [attr.required]="props.required"
+        [attr.disabled]="props.disabled ? true : null"
+        [autocapitalize]="props.autocapitalize"
+        [autofocus]="props.autofocus"
+        [autoGrow]="props.autoGrow"
+        [minlength]="props.minLength"
+        [maxlength]="props.maxLength"
+        [readonly]="props.readonly"
+        [spellcheck]="props.spellcheck"
+        [wrap]="props.wrap"
         [ngStyle]="props.styles?.input">
       </wcs-textarea>
     </formly-wcs-field-wrapper>
