fde729efd09af0aaa399c82dd98105105a43f308
Antoine Petot <antoine7104@gmail.com>
2024-12-20 14:07:27 +0000

    feat(field): migrate component on design tokens

--- src/components/field/field.tsx
+++ src/components/field/field.tsx
@@ -5,6 +5,20 @@ import { Component, ComponentInterface, h, Host, } from '@stencil/core';
  * 
  * @slot label Label of the field
  * @slot content Content of the field
+ *
+ * @cssprop --wcs-field-label-color - The color of the label
+ * @cssprop --wcs-field-label-font-weight - The font weight of the label
+ * @cssprop --wcs-field-label-font-size - The font size of the label
+ * @cssprop --wcs-field-label-line-height - The line height of the label
+ * 
+ * @cssprop --wcs-field-content-color - The color of the content
+ * @cssprop --wcs-field-content-font-weight - The font weight of the content
+ * @cssprop --wcs-field-content-font-size - The font size of the content
+ * 
+ * @cssprop --wcs-field-margin-top - The margin top of the field 
+ * @cssprop --wcs-field-margin-bottom - The margin bottom of the field
+ * @cssprop --wcs-field-gap - The gap between the label and the content
+ *
  */
 @Component({
     tag: 'wcs-field',
