1508aaf64c55bfc5686a4b8a43ba4c6b2c18a72f
Rémi Martinez <ext.remi.martinez@sncf.fr>
2025-01-20 15:43:27 +0100

    fix(field): design tokens mapping

--- src/components/field-content/field-content.tsx
+++ src/components/field-content/field-content.tsx
@@ -2,6 +2,11 @@ import { Component, ComponentInterface, h, Host } from '@stencil/core';
 
 /**
  * The field-content is a subcomponent of `wcs-field`. It represents the text content of the field.
+ * 
+ * @cssprop --wcs-field-content-color - Color of the content
+ * @cssprop --wcs-field-content-font-weight - Font weight of the content
+ * @cssprop --wcs-field-content-font-size - Font size of the content
+ * @cssprop --wcs-field-content-line-height - Line height of the content
  */
 @Component({
     tag: 'wcs-field-content',
--- src/components/field-label/field-label.tsx
+++ src/components/field-label/field-label.tsx
@@ -2,6 +2,11 @@ import { Component, ComponentInterface, h, Host, } from '@stencil/core';
 
 /**
  * The field-label is a subcomponent of `wcs-field`. It represents the label or title of the field.
+ * 
+ * @cssprop --wcs-field-label-color - Color of the label
+ * @cssprop --wcs-field-label-font-weight - Font weight of the label
+ * @cssprop --wcs-field-label-font-size - Font size of the label 
+ * @cssprop --wcs-field-label-line-height - Line height of the label
  */
 @Component({
     tag: 'wcs-field-label',
