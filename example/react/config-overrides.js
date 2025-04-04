b385ba3716dade924cba99482a6041acc8394e85
Mathis Poncet <math712b@gmail.com>
2025-04-04 17:07:35 +0200

    fix(internal): add alias for react module in config overrides (fix useRef error)

--- example/react/config-overrides.js
+++ example/react/config-overrides.js
@@ -1,7 +1,11 @@
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const path = require('path');
 
 module.exports = function override(config, env) {
     config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
+    config.resolve.alias = {
+        react: path.resolve('./node_modules/react')
+    };
 
     return config;
 };
